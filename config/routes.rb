18e5f5a4fb10eea20f870d4ebcb8c2b37ee19cb2
kentomiyazawa <kento4241@yahoo.co.jp>
2020-05-12 19:23:56 +0900

    諸々

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "teams#index"
+  root to: "homes#index"
 
   get '/teams/:id/chat', to: 'teams#chat'
   post "likes/:blog_id/create" => "likes#create"
