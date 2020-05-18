81835ebe375faeee4212f5327a5d901463ad6acd
kentomiyazawa <kento4241@yahoo.co.jp>
2020-05-18 15:44:44 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :comments
-  has_many :user_teams
-  has_many :teams, through: :user_teams
-  has_many :blogs
-  has_many :blogcomments
+  has_many :comments, dependent: :destroy
+  has_many :user_teams, dependent: :destroy
+  has_many :teams, through: :user_teams, dependent: :destroy
+  has_many :blogs, dependent: :destroy
+  has_many :blogcomments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :liked_blogs, through: :likes, source: :blog
   validates :name, presence: true, uniqueness: true       
