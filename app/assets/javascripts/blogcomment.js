777e6d01913f47f7748559338b24f491be50dde0
kentomiyazawa <kento4241@yahoo.co.jp>
2020-05-18 12:01:17 +0900

    Update blogcomment.js

--- app/assets/javascripts/blogcomment.js
+++ app/assets/javascripts/blogcomment.js
@@ -11,8 +11,7 @@ $(function(){
                       ${blogcomment.created_at}
                     </div>
                   </div>
-                </div>
-    `
+                </div>`
     return html;
   }
 
