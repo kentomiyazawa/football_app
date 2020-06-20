31fc57f7010798d9f00c466985d6895fde7de944
kentomiyazawa <kento4241@yahoo.co.jp>
2020-06-20 22:05:48 +0900

    image_setting

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -1,10 +1,15 @@
-require "refile/s3"
+require 'refile/s3'
 
-aws = {
-  access_key_id: "AWS_ACCESS_KEY_ID",
-  secret_access_key: "AWS_SECRET_ACCESS_KEY",
-  region: "ap-northeast-1",
-  bucket: "football-for-everyone",
-}
-Refile.cache = Refile::S3.new(prefix: "cache", **aws)
-Refile.store = Refile::S3.new(prefix: "store", **aws)
+if Rails.env.production? 
+  aws = {
+    access_key_id: ENV['S3_ACCESS_KEY_ID'], 
+    secret_access_key: ENV['S3_SECRET_ACCESS_KEY'], 
+    region: "ap-northeast-1",
+    bucket: "football-for-everyone"
+  }
+  Refile.cache = Refile::S3.new(prefix: 'cache', **aws)
+  Refile.store = Refile::S3.new(prefix: 'store', **aws)
+end
+
+
+
